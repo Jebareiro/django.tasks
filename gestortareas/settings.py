e895c456ef27dee23ae7728b7e06b8dac2921d86
Jebareiro <jessibareiroaponte@gmail.com>
2024-11-29 11:11:44 -0300

    Update settings.py

--- gestortareas/settings.py
+++ gestortareas/settings.py
@@ -138,5 +138,4 @@ LOGIN_URL = '/signin'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'tasks.CustomUser'  # Apunta a tu modelo de usuario personalizado
 
